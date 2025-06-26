5185aac9cd05a08132eab4367a58520d237830e8
Jumo1893 <jumo1893@gmail.com>
2025-06-26 19:50:56 +0200

    deletion for code review test

--- backend/src/main/java/de/hse/focusflow/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package de.hse.focusflow.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-  public ResourceNotFoundException(String message) {
-    super(message);
-  }
-}
